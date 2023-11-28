46a10bbd62dc03bfe5a1b366e52403bac14983b6
rodrigovalenteuea <rvp.snf23@uea.edu.br>
2023-11-28 19:49:02 -0400

    add initial menu

--- pongpygame/pong.py
+++ pongpygame/pong.py
@@ -9,12 +9,21 @@ COLOR_WHITE = (255, 255, 255)
 WIDTH = 1280
 HEIGHT = 720
 SCORE_MAX = 10
-PROBABILITY = 92
+PROBABILITY = 93
 
 size = (WIDTH, HEIGHT)
 pygame.init()
+
+#Initial Menu
+
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Pong - PyGame Edition - 2023-11-28")
+pygame.display.set_caption("Inicial Menu")
+option_play_font = pygame.font.Font('assets/PressStart2P.ttf',50)
+option_play_text = option_play_font.render('Press Space to Start!',True,COLOR_WHITE)
+option_play_text_rect = option_play_text.get_rect()
+option_play_text_rect.center = (screen.get_width()/2,360)
+screen.blit(option_play_text,option_play_text_rect)
+pygame.display.flip()
 
 # score text 1
 score_font = pygame.font.Font('assets/PressStart2P.ttf', 110)
@@ -41,7 +50,7 @@ victory_text_rect = score_text.get_rect()
 victory_text_rect.center = (550, 350)
 
 # lose text
-lose_text = result_font.render('LOSE', True, COLOR_WHITE, COLOR_BLACK)
+lose_text = result_font.render('DEFEAT', True, COLOR_WHITE, COLOR_BLACK)
 lose_text_rect = score_text.get_rect()
 lose_text_rect.center = (550, 350)
 
@@ -83,165 +92,178 @@ ball_position_x = 640
 ball_position_y = 360
 ball_speed_x_default = 6
 ball_speed_y_default = 6
-ball_speed_x = ball_speed_x_default
+ball_speed_x = ball_speed_x_default * random.choice([-1, 1])
 ball_speed_y = ball_speed_y_default
 ball_speed_extremity = 7
 ball_speed_middle = 5
 
 # game loop
-game_loop = True
 game_clock = pygame.time.Clock()
+running = True
 
-while game_loop:
-    var_sleep = 0
+while running:
     for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            exit()
-        if event.type == KEYDOWN:
-            if event.key == K_w:
-                player_1.moving_up = True
-            if event.key == K_s:
-                player_1.moving_down = True
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                pygame.display.flip()
+                pygame.display.set_caption("MyPong - PyGame Edition - 2023-12-12")
+                game_loop = True
 
-        if event.type == KEYUP:
-            if event.key == K_w:
-                player_1.moving_up = False
-            if event.key == K_s:
-                player_1.moving_down = False
+                while game_loop:
+                    var_sleep = 0
+                    for event in pygame.event.get():
+                        if event.type == QUIT:
+                            pygame.quit()
+                            exit()
+                        if event.type == KEYDOWN:
+                            if event.key == K_w:
+                                player_1.moving_up = True
+                            if event.key == K_s:
+                                player_1.moving_down = True
 
-    # checking the victory condition
-    if player_1.score < SCORE_MAX and player_2.score < SCORE_MAX:
-        screen.fill(COLOR_BLACK)
-        # player 1 collides with upper wall
-        if player_1.position_y <= 0:
-            player_1.position_y = 0
-        # player 1 collides with lower wall
-        elif player_1.position_y >= 660:
-            player_1.position_y = 660
-        # player 2 collides with upper wall
-        if player_2.position_y <= 0:
-            player_2.position_y = 0
-        # player 2 collides with lower wall
-        elif player_2.position_y >= 660:
-            player_2.position_y = 660
+                        if event.type == KEYUP:
+                            if event.key == K_w:
+                                player_1.moving_up = False
+                            if event.key == K_s:
+                                player_1.moving_down = False
+                        elif event.type == KEYDOWN:
+                            if event.key == K_ESCAPE:
+                                pygame.display.quit()
 
-        # ball collision with the players
-        if ball_position_y <= player_2.position_y + 60 and ball_position_y + 12 >= player_2.position_y:
-            if ball_position_x + 12 >= player_2.position_x:
-                bounce_table_sound_effect.play()
-                ball_speed_y = ball_speed_extremity
-                ball_speed_x = ball_speed_extremity
-                l1 = player_2.position_y + 28
-                l2 = player_2.position_y + 32
-                if (ball_speed_y > 0 and ball_position_y + 11 < l1) or (ball_speed_y < 0 and ball_position_y > l2):
-                    ball_speed_y *= - 1
-                elif ball_position_y + 11 < l1:
-                    ball_speed_y = -1
-                elif ball_position_y > l2:
-                    ball_speed_y = ball_speed_y_default
-                else:
-                    ball_speed_y = 0
-                    ball_speed_x = ball_speed_middle
+                    # checking the victory condition
+                    if player_1.score < SCORE_MAX and player_2.score < SCORE_MAX:
+                        screen.fill(COLOR_BLACK)
+                        # player 1 collides with upper wall
+                        if player_1.position_y <= 0:
+                            player_1.position_y = 0
+                        # player 1 collides with lower wall
+                        elif player_1.position_y >= 660:
+                            player_1.position_y = 660
+                        # player 2 collides with upper wall
+                        if player_2.position_y <= 0:
+                            player_2.position_y = 0
+                        # player 2 collides with lower wall
+                        elif player_2.position_y >= 660:
+                            player_2.position_y = 660
 
-                if ball_speed_x > 0:
-                    ball_speed_x *= -1
+                        # ball collision with the players
+                        if ball_position_y <= player_2.position_y + 60 and ball_position_y + 12 >= player_2.position_y:
+                            if ball_position_x + 12 >= player_2.position_x:
+                                bounce_table_sound_effect.play()
+                                ball_speed_y = ball_speed_extremity
+                                ball_speed_x = ball_speed_extremity
+                                l1 = player_2.position_y + 28
+                                l2 = player_2.position_y + 32
+                                if (ball_speed_y > 0 and ball_position_y + 11 < l1) or (ball_speed_y < 0 and ball_position_y > l2):
+                                    ball_speed_y *= - 1
+                                elif ball_position_y + 11 < l1:
+                                    ball_speed_y = -1
+                                elif ball_position_y > l2:
+                                    ball_speed_y = ball_speed_y_default
+                                else:
+                                    ball_speed_y = 0
+                                    ball_speed_x = ball_speed_middle
 
-        if ball_position_y <= player_1.position_y + 60 and ball_position_y + 12 >= player_1.position_y:
-            if ball_position_x <= player_1.position_x + 14:
-                bounce_table_sound_effect.play()
-                ball_speed_y = ball_speed_extremity
-                ball_speed_x = ball_speed_extremity
-                l1 = player_1.position_y + 28
-                l2 = player_1.position_y + 32
-                if (ball_speed_y > 0 and ball_position_y + 11 < l1) or (ball_speed_y < 0 and ball_position_y > l2):
-                    ball_speed_y *= - 1
-                elif ball_position_y + 11 < l1:
-                    ball_speed_y = -1
-                elif ball_position_y > l2:
-                    ball_speed_y = ball_speed_y_default
-                else:
-                    ball_speed_y = 0
+                                if ball_speed_x > 0:
+                                    ball_speed_x *= -1
 
-                if ball_speed_x < 0:
-                    ball_speed_x = ball_speed_middle
-                    ball_speed_x *= -1
+                        if ball_position_y <= player_1.position_y + 60 and ball_position_y + 12 >= player_1.position_y:
+                            if ball_position_x <= player_1.position_x + 14:
+                                bounce_table_sound_effect.play()
+                                ball_speed_y = ball_speed_extremity
+                                ball_speed_x = ball_speed_extremity
+                                l1 = player_1.position_y + 28
+                                l2 = player_1.position_y + 32
+                                if (ball_speed_y > 0 and ball_position_y + 11 < l1) or (ball_speed_y < 0 and ball_position_y > l2):
+                                    ball_speed_y *= - 1
+                                elif ball_position_y + 11 < l1:
+                                    ball_speed_y = -1
+                                elif ball_position_y > l2:
+                                    ball_speed_y = ball_speed_y_default
+                                else:
+                                    ball_speed_y = 0
 
-        # ball collision with the wall
-        if ball_position_y > 700 and ball_speed_y > 0:
-            ball_speed_y *= -1
-            bounce_wall_sound_effect.play()
-        elif ball_position_y <= 0 and ball_speed_y < 0:
-            ball_speed_y *= -1
-            bounce_wall_sound_effect.play()
+                                if ball_speed_x < 0:
+                                    ball_speed_x = ball_speed_middle
+                                    ball_speed_x *= -1
 
-        # probability
-        prob = random.randint(1, 100)
-        if player_2.position_y > ball_position_y:
-            if prob <= PROBABILITY:
-                player_2.position_y -= player_2.speed_y
-        if player_2.position_y + 60 < ball_position_y + 12:
-            if prob <= PROBABILITY:
-                player_2.position_y += player_2.speed_y
+                        # ball collision with the wall
+                        if ball_position_y > 700 and ball_speed_y > 0:
+                            ball_speed_y *= -1
+                            bounce_wall_sound_effect.play()
+                        elif ball_position_y <= 0 and ball_speed_y < 0:
+                            ball_speed_y *= -1
+                            bounce_wall_sound_effect.play()
 
-        # scoring points
-        if ball_position_x < 38:
-            direction_random = random.choice([-1, 1])
-            ball_speed_y = ball_speed_y_default * direction_random
-            ball_speed_x = ball_speed_x_default
-            if ball_speed_x > 0:
-                ball_speed_x *= -1
-            ball_position_x = 640
-            ball_position_y = random.randint(0, 720)
-            player_2.score += 1
-            scoring_sound_effect.play()
-            var_sleep = 1
+                        # probability
+                        prob = random.randint(1, 100)
+                        if player_2.position_y > ball_position_y:
+                            if prob <= PROBABILITY:
+                                player_2.position_y -= player_2.speed_y
+                        if player_2.position_y + 60 < ball_position_y + 12:
+                            if prob <= PROBABILITY:
+                                player_2.position_y += player_2.speed_y
 
-        elif ball_position_x > 1244:
-            direction_random = random.choice([-1, 1])
-            ball_speed_y = ball_speed_y_default * direction_random
-            ball_speed_x = ball_speed_x_default
-            if ball_speed_x < 0:
-                ball_speed_x *= -1
-            ball_position_x = 640
-            ball_position_y = random.randint(0, 720)
-            player_1.score += 1
-            PROBABILITY += 2
-            scoring_sound_effect.play()
-            var_sleep = 1
-        # player 1 movement
-        if player_1.moving_up:
-            player_1.position_y -= player_1.speed_y
-        if player_1.moving_down:
-            player_1.position_y += player_1.speed_y
+                        # scoring points
+                        if ball_position_x < 38:
+                            direction_random = random.choice([-1, 1])
+                            ball_speed_y = ball_speed_y_default * direction_random
+                            ball_speed_x = ball_speed_x_default
+                            if ball_speed_x > 0:
+                                ball_speed_x *= -1
+                            ball_position_x = 640
+                            ball_position_y = random.randint(0, 720)
+                            player_2.score += 1
+                            scoring_sound_effect.play()
+                            var_sleep = 1
 
-        # ball movement
-        ball_position_x = ball_position_x + ball_speed_x
-        ball_position_y = ball_position_y + ball_speed_y
+                        elif ball_position_x > 1244:
+                            direction_random = random.choice([-1, 1])
+                            ball_speed_y = ball_speed_y_default * direction_random
+                            ball_speed_x = ball_speed_x_default
+                            if ball_speed_x < 0:
+                                ball_speed_x *= -1
+                            ball_position_x = 640
+                            ball_position_y = random.randint(0, 720)
+                            player_1.score += 1
+                            PROBABILITY += 2
+                            scoring_sound_effect.play()
+                            var_sleep = 1
+                        # player 1 movement
+                        if player_1.moving_up:
+                            player_1.position_y -= player_1.speed_y
+                        if player_1.moving_down:
+                            player_1.position_y += player_1.speed_y
 
-        # update score hud
-        score_text = score_font.render(str(player_1.score), True, COLOR_WHITE, COLOR_BLACK)
-        score_text2 = score_font.render(str(player_2.score), True, COLOR_WHITE, COLOR_BLACK)
-        screen.blit(ball, (ball_position_x, ball_position_y))
-        screen.blit(score_text, score_text_rect)
-        screen.blit(score_text2, score_text_rect2)
-        screen.blit(player_1_img, (player_1.position_x, player_1.position_y))
-        screen.blit(player_2_img, (player_2.position_x, player_2.position_y))
-        for cont in range(1, 20):
-            screen.blit(middle_bar, (middle_bar_center, middle_bar_y * cont))
-            middle_bar_y = 40
-    else:
-        # drawing victory
-        screen.fill(COLOR_BLACK)
-        screen.blit(score_text, score_text_rect)
-        screen.blit(score_text2, score_text_rect2)
-        if player_1.score>player_2.score:
-            screen.blit(victory_text, victory_text_rect)
-        else:
-            screen.blit(lose_text, lose_text_rect)
-    pygame.display.flip()
-    game_clock.tick(75)
-    if var_sleep == 1:
-        player_1.position_y = 300
-        player_2.position_y = 300
-        time.sleep(0.5)
+                        # ball movement
+                        ball_position_x = ball_position_x + ball_speed_x
+                        ball_position_y = ball_position_y + ball_speed_y
+
+                        # update score hud
+                        score_text = score_font.render(str(player_1.score), True, COLOR_WHITE, COLOR_BLACK)
+                        score_text2 = score_font.render(str(player_2.score), True, COLOR_WHITE, COLOR_BLACK)
+                        screen.blit(ball, (ball_position_x, ball_position_y))
+                        screen.blit(score_text, score_text_rect)
+                        screen.blit(score_text2, score_text_rect2)
+                        screen.blit(player_1_img, (player_1.position_x, player_1.position_y))
+                        screen.blit(player_2_img, (player_2.position_x, player_2.position_y))
+                        for cont in range(1, 20):
+                            screen.blit(middle_bar, (middle_bar_center, middle_bar_y * cont))
+                            middle_bar_y = 40
+                    else:
+                        # drawing victory
+                        screen.fill(COLOR_BLACK)
+                        screen.blit(score_text, score_text_rect)
+                        screen.blit(score_text2, score_text_rect2)
+                        if player_1.score>player_2.score:
+                            screen.blit(victory_text, victory_text_rect)
+                        else:
+                            screen.blit(lose_text, lose_text_rect)
+                    pygame.display.flip()
+                    game_clock.tick(75)
+                    if var_sleep == 1:
+                        player_1.position_y = 300
+                        player_2.position_y = 300
+                        time.sleep(0.5)
